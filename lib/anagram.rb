910450a07f6c9b7630444023591151a571ee429b
stacysorey <stacysorey@gmail.com>
2020-11-20 19:41:54 -0500

    commit tree 88ec8e4ff3c98927943b80800fc9eee056a0f2c6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+    @word = word
+    end 
+
+    def match(maybe_anagram)
+    maybe_anagram.select {|a| a.split("").sort == @word.split("").sort}
+    end
+  
+end
+
